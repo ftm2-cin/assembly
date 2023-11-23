b5b272b39fab35eabac5ada258ef65dd5cdb152d
João Marcelo <jmsf3@cin.ufpe.br>
2023-11-22 21:15:13 -0300

    Refatorações

--- Lista 1/q1a.asm
+++ Lista 1/q1a.asm
@@ -1,25 +1,22 @@
-main:
-    sw x0, x # x = 0
-
-    lw x5, a # Carregar o valor de 'a'
-    lw x6, a # Carregar o valor de 'b'
-    lw x7, a # Carregar o valor de 'c'
-
-    check:
-        addi x6, x6, 0xffffffb4 # Subtrair 76 de 'b'
-        addi x7, x7, 0xffffffe6 # Subtrair 26 de 'c'
-
-        blt x5, x0, skip # a >= 0?
-        bge x6, x0, skip # b < 75?
-        blt x7, x0, skip # c > 25?
-
-        addi x5, x0, 1
-        sw x5, x # x = 1
-    skip:
-
-    halt # Encerrar o programa
-
-a: .word 0x00000000 # int a;
-b: .word 0x00000000 # int b;
-c: .word 0x00000000 # int c;
-x: .word 0x00000000 # int x;
+begin:
+    sw    x0, x              # x = 0
+    lw    x5, a              # Carregar o valor de 'a'
+    lw    x6, b              # Carregar o valor de 'b'
+    lw    x7, c              # Carregar o valor de 'c'
+    addi  x6, x6, 0xffffffb4 # Subtrair 76 de 'b'
+    addi  x7, x7, 0xffffffe6 # Subtrair 26 de 'c'
+    blt   x5, x0, end        # a >= 0?
+    bge   x6, x0, end        # b < 75?
+    blt   x7, x0, end        # c > 25?
+    addi  x5, x0, 0x00000001 # x = 1
+    sw    x5, x              # Salvar o valor de 'x' na memória
+end:
+    halt                     # Encerrar o programa
+a:
+    .word 0x00000000         # Salvar 'a' como variável na memória
+b:
+    .word 0x00000040         # Salvar 'b' como variável na memória
+c:
+    .word 0x00000030         # Salvar 'c' como variável na memória
+x:
+    .word 0x000000ff         # Salvar 'x' como variável na memória
